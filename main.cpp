acc5164ae03199c4acf40324ded1bd3c62a91bf0
Yves Baumes <ybaumes@gmail.com>
2015-08-26 22:18:36 +0200

    Implement displaying background image.

--- main.cpp
+++ main.cpp
@@ -11,7 +11,9 @@ int main()
     SDL_Window *display = nullptr;
     SDL_Renderer *ren = nullptr;
     SDL_Surface *bmp = nullptr;
-    SDL_Texture *tex = nullptr;
+    SDL_Texture *background = nullptr;
+    SDL_Texture *image = nullptr;
+    SDL_Rect dst;
 
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 	cerr << "SDL_Init error: " << SDL_GetError() << "." << endl;
@@ -35,29 +37,71 @@ int main()
 	goto clean;
     }
 
-    bmp = SDL_LoadBMP("./hello.bmp");
+    bmp = SDL_LoadBMP("./background.bmp");
     if(!bmp) {
-	cerr << "SDL_LoadBMP() error : " << SDL_GetError() << "." << endl;
+	cerr << "SDL_LoadBMP() error : " << SDL_GetError() << endl;
 	res = 1;
 	goto clean;
     }
 
-    tex = SDL_CreateTextureFromSurface(ren, bmp);
-    if(!tex) {
+    background = SDL_CreateTextureFromSurface(ren, bmp);
+    if(!background) {
 	cerr << "SDL_CreateTextureFromSurface() error : " << SDL_GetError() << "." << endl;
 	res = 1;
 	goto clean;
     }
 
     SDL_FreeSurface(bmp);
+    bmp = nullptr;
+
+    bmp = SDL_LoadBMP("./image.bmp");
+    if(!bmp) {
+	cerr << "SDL_LoadBMP() error : " << SDL_GetError() << endl;
+	res = 1;
+	goto clean;
+    }
+
+    image = SDL_CreateTextureFromSurface(ren, bmp);
+    if(!image) {
+	cerr << "SDL_CreateTextureFromSurface() error : " << SDL_GetError() << "." << endl;
+	res = 1;
+	goto clean;
+    }
+
+    SDL_FreeSurface(bmp);
+    bmp = nullptr;
+
+    res = SDL_QueryTexture(background, nullptr, nullptr, &dst.w, &dst.h);
+    if(res != 0) {
+	cerr << "SDL_QueryTexture() error : " << SDL_GetError() << "." << endl;
+	goto clean;
+    }
+
     for(int i = 0; i < 3; i++) {
 	SDL_RenderClear(ren);
-	SDL_RenderCopy(ren, tex, nullptr, nullptr);
+
+	dst.x = dst.y = 0;
+	SDL_RenderCopy(ren, background, nullptr, &dst);
+	dst.x = dst.w;
+	dst.y = 0;
+	SDL_RenderCopy(ren, background, nullptr, &dst);
+	dst.x = 0;
+	dst.y = dst.h;
+	SDL_RenderCopy(ren, background, nullptr, &dst);
+	dst.x = dst.w;
+	dst.y = dst.h;
+	SDL_RenderCopy(ren, background, nullptr, &dst);
+
 	SDL_RenderPresent(ren);
+
 	SDL_Delay(1000);
     }
 
  clean:
+    if(image)
+	SDL_DestroyTexture(image);
+    if(background)
+	SDL_DestroyTexture(background);
     if(bmp)
 	SDL_FreeSurface(bmp);
     if(ren)
@@ -68,4 +112,4 @@ int main()
     return res;
 }
 
-// g++ -std=c++11 main.cpp $(sdl2-config --cflags --libs)
+// g++ -ggdb -std=c++11 main.cpp $(sdl2-config --cflags --libs)
