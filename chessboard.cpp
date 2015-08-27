3b3d21dd3a8cf68dff913a62aeb33e1531306662
Yves Baumes <ybaumes@gmail.com>
2015-08-27 22:53:37 +0200

    Implement paint_chess_board() function.

--- chessboard.cpp
+++ chessboard.cpp
@@ -4,15 +4,48 @@
 
 using namespace std;
 
+SDL_Window *display = nullptr;
+SDL_Renderer *ren = nullptr;
+
+void paint_chess_board()
+{
+    SDL_Rect dst, viewport;
+
+    SDL_SetRenderDrawColor(ren, 0, 0, 0, SDL_ALPHA_OPAQUE);
+    SDL_RenderClear(ren);
+
+    SDL_RenderGetViewport(ren, &viewport);
+
+    dst.x = dst.y = 0;
+    dst.w = viewport.w / 8;
+    dst.h = viewport.h / 8;
+    for(int c = 0; c < 8; c++)
+    {
+	dst.x = 0;
+	for(int r = 0; r < 8; r++)
+	{
+	    if((c % 2 == 0 && r % 2 == 0) || (c % 2 == 1 && r % 2 == 1))
+		SDL_SetRenderDrawColor(ren, 255, 255, 255, SDL_ALPHA_OPAQUE); // White square
+	    else
+		SDL_SetRenderDrawColor(ren, 255, 0, 0, SDL_ALPHA_OPAQUE); // Dark square
+
+	    // printf("dst.x=%d, dst.y=%d, dst.w=%d, dst.h=%d.\n",
+	    //        dst.x, dst.y, dst.w, dst.h);
+	    SDL_RenderFillRect(ren, &dst);
+	    dst.x += dst.w;
+	}
+	dst.y += dst.h;
+    }
+    SDL_RenderPresent(ren);
+    SDL_UpdateWindowSurface(display);
+}
+
 int main()
 {
     int res = 0;
     constexpr Uint32 renderer_flags = 0;
-    SDL_Window *display = nullptr;
-    SDL_Renderer *ren = nullptr;
-    SDL_Rect dst;
     constexpr int screenwidth = 640;
-    constexpr int screenheigh = 480;
+    constexpr int screenheigh = 640;
 
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0) {
 	cerr << "SDL_Init error: " << SDL_GetError() << "." << endl;
@@ -37,14 +70,7 @@ int main()
     }
 
     for(int i = 0; i < 3; i++) {
-	SDL_RenderClear(ren);
-
-	dst.x = dst.y = 0;
-	dst.w = dst.h = 30;
-	// SDL_RenderCopy(ren, square, nullptr, &dst);
-
-	SDL_RenderPresent(ren);
-
+	paint_chess_board();
 	SDL_Delay(1000);
     }
 
