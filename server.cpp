8eef7517fcc491f413146095c10bd2d5ecf9ca01
Yves Baumes <ybaumes@gmail.com>
2015-09-05 18:22:23 +0200

    Implement reject move msg.

--- server.cpp
+++ server.cpp
@@ -138,7 +138,6 @@ void process_move(int fd, struct move_msg *move_msg)
     }
     else
     {
-        // TODO Reject the move by the player originating the move.
         struct reject_move_msg reject_move_msg;
         reject_move_msg.msg_type = msg_type::reject_move_msg;
         ssize_t n = send(fd, &reject_move_msg, sizeof(reject_move_msg), 0);
